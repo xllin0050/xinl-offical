fd47eab23b63d8bf66a9c5536b89cb073c157fd6
Rd-Carl <carl@6400i5.local>
2025-02-21 11:21:08 +0800

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import '../app.css';
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	let { children } = $props();
+</script>
+
+<div class="bg-background image font-iceland grid min-h-svh place-items-center py-0 md:py-10">
+	<ParaglideJS {i18n}>
+		{@render children()}
+	</ParaglideJS>
+</div>
+<footer class="text-middleground absolute bottom-0 right-0 uppercase px-4">
+	<p>&copy; {new Date().getFullYear()} xinl</p>
+</footer>
+
+<style>
+	.image {
+		background-image: url(/omg41.jpg);
+		background-size: cover;
+		background-position: bottom;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<main class="bg-background text-foreground h-full w-full px-8 opacity-80 md:w-xl">
+	<section class="">
+		<h1 class="text-[4rem] uppercase md:text-[8rem]">xinl</h1>
+	</section>
+	<section class="">
+		<h2 class="text-2xl uppercase">about</h2>
+	</section>
+	<section class="">
+		<h2 class="text-2xl uppercase">release</h2>
+	</section>
+	<section class="">
+		<h2 class="text-2xl uppercase">contact</h2>
+	</section>
+</main>
