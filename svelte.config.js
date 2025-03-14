363bbcb8cf693ba9a932b307a0606dfce0e387d8
Xinliang Carl <xinliang@localhost>
2025-03-15 00:52:18 +0800

    fix: nodejs compat

--- svelte.config.js
+++ svelte.config.js
@@ -11,15 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-	adapter: adapter({
-	  // 保留 Node.js 兼容性模式
-      nodejs_compat: true,
-      // 添加路由配置，优化静态资源处理
-      routes: {
-        include: ['/*'],
-        exclude: ['<all>'] // 使用默认排除所有静态资源
-      }
-	})
+		adapter: adapter({})
 	}
 };
 
