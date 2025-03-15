09e988b0fe30bc5f30c05f0dd07feb05aa64b38f
Xinliang Carl <xinliang@localhost>
2025-03-15 10:18:03 +0800

    fix: hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,6 @@
 import type { Handle } from '@sveltejs/kit';
 import { i18n } from '$lib/i18n';
-const handleParaglide: Handle = i18n.handle();
+const handleParaglide: Handle = i18n.handle({
+	disableAsyncLocalStorage: true // @default = false
+});
 export const handle: Handle = handleParaglide;
